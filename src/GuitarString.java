97a931bbd5ff932774692740f6dc3897de141a59
GuitarJack1 <tigerjack07@icloud.com>
2023-11-09 12:24:16 -0600

    Added constructors

--- src/GuitarString.java
+++ src/GuitarString.java
@@ -1,10 +1,21 @@
 
 public class GuitarString {
+	int samplingRate = 44100;
+	RingBuffer rb;
+	
 	public GuitarString(double frequency){
 		// use a sampling rate of 44,100
+		rb = new RingBuffer((int)Math.ceil((samplingRate / frequency)));
+		for (int i = 0; i < (int)Math.ceil((samplingRate / frequency)); i++) {
+			rb.enqueue(0.0);
+		}
 	}
 	public GuitarString(double[] init){
 		// size and values are given by the array
+		rb = new RingBuffer(init.length);
+		for (double i : init) {
+			rb.enqueue(i);
+		}
 	}
 	public void pluck() {
 		// set the buffer to white noise
