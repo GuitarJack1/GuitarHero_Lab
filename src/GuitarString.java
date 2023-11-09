8e908c441bfbcb4211975b475e7c81eb61c790f3
GuitarJack1 <tigerjack07@icloud.com>
2023-11-09 12:17:23 -0600

    Made methods public

--- src/GuitarString.java
+++ src/GuitarString.java
@@ -1,23 +1,23 @@
 
 public class GuitarString {
-	GuitarString(double frequency){
+	public GuitarString(double frequency){
 		// use a sampling rate of 44,100
 	}
-	GuitarString(double[] init){
+	public GuitarString(double[] init){
 		// size and values are given by the array
 	}
-	void pluck() {
+	public void pluck() {
 		// set the buffer to white noise
 	}
-	void tic() {
+	public void tic() {
 		// advance the simulation one time step
 	}
-	double sample() {
+	public double sample() {
 		// return the current sample
 		
 		return 0.0;
 	}
-	int time() {
+	public int time() {
 		// return number of tics
 		
 		return 0;
