a8dce2ffb530a491c9578ed7c821a31b648a4763
GuitarJack1 <tigerjack07@icloud.com>
2023-11-09 12:33:00 -0600

    Added time, and sample

--- src/GuitarString.java
+++ src/GuitarString.java
@@ -2,6 +2,8 @@
 public class GuitarString {
 	int samplingRate = 44100;
 	RingBuffer rb;
+	int tics = 0;
+	int size;
 	
 	public GuitarString(double frequency){
 		// use a sampling rate of 44,100
@@ -9,6 +11,7 @@ public class GuitarString {
 		for (int i = 0; i < (int)Math.ceil((samplingRate / frequency)); i++) {
 			rb.enqueue(0.0);
 		}
+		size = (int)Math.ceil((samplingRate / frequency));
 	}
 	public GuitarString(double[] init){
 		// size and values are given by the array
@@ -16,22 +19,24 @@ public class GuitarString {
 		for (double i : init) {
 			rb.enqueue(i);
 		}
+		size = init.length;
 	}
 	public void pluck() {
 		// set the buffer to white noise
 	}
 	public void tic() {
 		// advance the simulation one time step
+		tics++;
 	}
 	public double sample() {
 		// return the current sample
 		
-		return 0.0;
+		return rb.peek();
 	}
 	public int time() {
 		// return number of tics
 		
-		return 0;
+		return tics;
 	}
 
 }
